fda0ecda6e5c3039ab7c8a1deda76c32813af0cc
Grayson Shogan <graysontoddshogan@gmail.com>
2023-05-01 15:55:57 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "./actions";
+import "./App.css";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -11,12 +13,12 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div>
+    <div className="App">
       {data &&
         data.map((item) => (
-          <div key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.body}</p>
+          <div key={item.id} className="App-item">
+            <h2 className="App-title">{item.title}</h2>
+            <p className="App-body">{item.body}</p>
           </div>
         ))}
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./App";
+import "./App.css";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
